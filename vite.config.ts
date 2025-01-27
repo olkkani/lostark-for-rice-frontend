899affafe001ec00c79362fa57e42ab9ff12a243
acj <ahn.chanjin.96@gmail.com>
2025-01-27 13:24:56 +0900

    errortest add expose port 5173 in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,27 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig, loadEnv } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [react()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
-        '@assets': path.resolve(__dirname, './src/assets'),
+        "@assets": path.resolve(__dirname, "./src/assets"),
       },
     },
     server: {
+      port: 5173,
       proxy: {
-        '/items': {
+        "/items": {
           target: env.VITE_APP_API_URL,
           // rewrite: (path) => path.replace(/^\/api/, ''),
           changeOrigin: true,
           secure: false,
-        }
-      }
-    }
-  }
-})
+        },
+      },
+    },
+  };
+});
