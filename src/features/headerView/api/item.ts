8370b5c5698cae193ceb3dd353b67b33b7ebe43e
acj <ahn.chanjin.96@gmail.com>
2025-02-10 10:12:41 +0900

    change folder structure to FSD

---
+++ src/features/headerView/api/item.ts
@@ -0,0 +1,41 @@
+import ky from "ky";
+import {
+  ItemPreviewResponse,
+  ItemPricesResponse,
+} from "@/features/headerView/model/ItemResponse";
+
+export interface ItemResponse {
+  itemCode: number;
+  prices: {
+    open: number;
+    close: number;
+    high: number;
+    low: number;
+  };
+}
+
+export async function getAllKindsItemPrice() {
+  const response = await ky(`/items/prices/today`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Fetch error: ${response.statusText}`);
+  }
+  return await response.json<ItemPreviewResponse[]>();
+}
+
+export async function getPricesByItemCode(itemCode: number) {
+  const response = await ky(`/items/${itemCode}/prices`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Fetch error: ${response.statusText}`);
+  }
+  return await response.json<ItemPricesResponse[]>();
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import {QueryClientProvider, QueryClient} from '@tanstack/react-query';
-import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import './index.css'
-import App from './App.tsx'
+import "./app/index.css";
+import App from "./app/App.tsx";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <App/>
-            <ReactQueryDevtools initialIsOpen={true}/>
-        </QueryClientProvider>
-    </StrictMode>
-)
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  </StrictMode>
+);
