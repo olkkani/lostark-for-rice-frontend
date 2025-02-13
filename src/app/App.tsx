7972e7dd475d4cdb821d781905a49062a30efc2a
acj <ahn.chanjin.96@gmail.com>
2025-02-13 15:30:45 +0900

    test serve

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import GemSPI from "@/pages/GemSPI.tsx";
 
 import "./App.css";
+import Hello from "@/pages/hello";
 
-const router = createBrowserRouter([{ path: "/", element: <GemSPI /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <GemSPI /> },
+  { path: "/hello", element: <Hello /> },
+]);
 function App() {
   return (
     // <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
